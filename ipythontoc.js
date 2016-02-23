6ed0719d8688ef934c0fc496d137b3ce4f3270a6
HugoGuillen <HugoGuillen@users.noreply.github.com>
2016-02-22 19:40:21 -0800

    Update ipythontoc.js

--- ipythontoc.js
+++ ipythontoc.js
@@ -10,20 +10,6 @@ Step 2: Add a code cell anywhere in the notebook with the following:
 $.getScript('https://kmahelona.github.io/ipython_notebook_goodies/ipython_notebook_toc.js')
 */
 
-// Converts integer to roman numeral
-function romanize(num) {
-    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
-	roman = '',
-	    i;
-	for ( i in lookup ) {
-	    while ( num >= lookup[i] ) {
-		roman += i;
-		num -= lookup[i];
-	    }
-	}
-	return roman;
- }
-
 // Builds a <ul> Table of Contents from all <headers> in DOM
 function createTOC(){
     var toc = "";
@@ -56,8 +42,6 @@ function createTOC(){
 	    if (this.id==''){this.id = this.innerHTML.replace(/ /g,"-")}
 	    var anchor = this.id;
         
-	    /*toc += '<li><a href="#' + anchor + '">' +  romanize(levels[openLevel].toString()) + '. ' + titleText
-		+ '</a></li>';*/
 		toc += '<li><a href="#' + anchor + '">' +  levels[openLevel].toString() + '. ' + titleText
 		+ '</a></li>';      
 		
@@ -78,6 +62,3 @@ function createTOC(){
 
 // Executes the createToc function
 setTimeout(function(){createTOC();},100);
-
-// Rebuild to TOC every minute
-// setInterval(function(){createTOC();},60000);
